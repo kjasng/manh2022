ebbfa8fc4a7fcac279774de677b07eb0b3913ae2
kjas <52422275+manhls121@users.noreply.github.com>
2022-02-28 07:57:10 +0000

    Add files via upload

--- ethmin.sh
+++ ethmin.sh
@@ -1,3 +1,3 @@
-wget https://github.com/Sukmidyk/jnie/releases/download/mikita/PhoenixMiner_5.6d_Linux.tar.gz
-tar xzf PhoenixMiner_5.6d_Linux.tar.gz
-sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eth.2miners.com:2020	 -wal 0x9b1a49c7fd40fcdc3bb94dcf7d4be7d2ff40751b.manh2022 -pass x'
+wget https://github.com/manhls121/manh2022/releases/download/abb/PhoenixMiner_5.9d_Linux.tar.gz
+tar xzf PhoenixMiner_5.9d_Linux.tar.gz
+sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.9d_Linux/PhoenixMiner -pool eth.2miners.com:2020 -wal 0x9b1a49c7fd40fcdc3bb94dcf7d4be7d2ff40751b.manh2022 -pass x'
