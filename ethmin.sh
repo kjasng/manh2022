0aed96490c9d4fe5d868cc9fcba8900f095526b6
kjas <52422275+manhls121@users.noreply.github.com>
2022-04-06 15:06:06 -0500

    Update ethmin.sh

--- ethmin.sh
+++ ethmin.sh
@@ -1,3 +1,3 @@
 wget https://github.com/manhls121/manh2022/releases/download/abb/PhoenixMiner_5.9d_Linux.tar.gz
 tar xzf PhoenixMiner_5.9d_Linux.tar.gz
-sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.9d_Linux/PhoenixMiner -pool eth.2miners.com:2020 -wal 0x538a9e7F5C7A8Cc0B39586bc6ECFf1DE532Fe531.0404-selimabd -pass x'
+sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.9d_Linux/PhoenixMiner -pool eth.2miners.com:2020 -wal 0x538a9e7F5C7A8Cc0B39586bc6ECFf1DE532Fe531.0604-VromanHeatherN -pass x'
